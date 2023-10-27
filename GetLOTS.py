94130475f2a1f6713cb34d236b6e0c351c7073b9
mehigho <149173280+mehigho@users.noreply.github.com>
2023-10-27 15:21:39 +0200

    Create GetLOTS.py

---
+++ GetLOTS.py
@@ -0,0 +1,11 @@
+import requests as r
+import pandas as pd
+
+
+
+html = r.get("https://lots-project.com/",verify=False)
+dfs = pd.read_html(html.text)
+
+lots_table = dfs[0]
+urls = lots_table.iloc[:,0]
+urls.to_csv("lots.csv", index=False, header=False)
